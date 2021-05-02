6f01c9543f60ebc8f8a9035f03b0478e7656c602
Standalone <Standalone@DL00100.internal.uwic.ac.uk>
2021-05-02 09:22:13 +0100

    second commit

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Output, Input
 import dash_bootstrap_components as dbc
-import plotly.graph_objects as go
 
 VALID_USERNAME_PASSWORD_PAIRS = {
     'user': 'Password1!'
@@ -25,45 +24,6 @@ df.sort_values('Type', inplace=True)
 
 dfa = pd.read_csv("ethics.csv")
 dfa.sort_values(by=['Health of Sport?'], inplace=True)
-
-dfb = pd.read_csv("ethics.csv")
-low = dfb['ID'].tolist()
-high = np.array(dfb['ID'])+20
-
-trace1 = go.Scatter(x=dfb.Created[:2],
-                    y=low[:2],
-                    mode='lines',
-                    line=dict(width=3))
-
-frames = [dict(data= [dict(type='scatter',
-                           x=dfb.Created[:k+1],
-                           y=low[:k+1])],
-               traces= [0], 
-              )for k  in  range(1, len(low)-1)] 
-
-layout = go.Layout(width=1200,
-                   height=500,
-                   showlegend=False,
-                   hovermode='closest',
-                   updatemenus=[dict(type='buttons', showactive=False,
-                                y=1.05,
-                                x=1.15,
-                                xanchor='right',
-                                yanchor='top',
-                                pad=dict(t=0, r=10),
-                                buttons=[dict(label='Start',
-                                              method='animate',
-                                              args=[None, 
-                                                    dict(frame=dict(duration=1, 
-                                                                    redraw=False),
-                                                         transition=dict(duration=0),
-                                                         fromcurrent=True,
-                                                         mode='immediate')])])])
-
-
-layout.update(xaxis =dict(range=[dfb.Created[0], dfb.Created[len(dfb)-1]], autorange=False),
-              yaxis =dict(range=[min(low)-0.5, high.max()+0.5], autorange=False));
-fig = go.Figure(data=[trace1], frames=frames, layout=layout)
 
 app.layout=html.Div([
         html.Center(
@@ -103,12 +63,7 @@ app.layout=html.Div([
                     label="Type of Approval Sought",
                     children=[
                         dcc.Graph(id='my-graph6', figure=px.histogram(data_frame=df,x="Health of Sport?",color="Approval Requirement By"))]),
-                dbc.Tab(
-                    label="Total applications (animated)",
-                    children=[
-                        dcc.Graph(id="graph", figure=fig),
             ])])
-])
 
 @app.callback(
     Output(component_id='the-graph', component_property='figure'),
@@ -122,8 +77,6 @@ def update_graph(options_chosen):
     histochart=px.histogram(data_frame=dff, x="Health of Sport?",color="Type",barmode='group')
 
     return (histochart)
-
-
 
 if __name__=='__main__':
     app.run_server()
